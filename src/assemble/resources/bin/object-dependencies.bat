64b8e9f6ff8535790573837f9cb47e3a6588c2db
Francois DELOYE <francois.deloye@non.schneider-electric.com>
2014-07-07 14:41:43 +0200

    Reworked mind-violation and object-dependencies launchers to use jar_launcher, added .bat launcher

---
+++ src/assemble/resources/bin/object-dependencies.bat
@@ -0,0 +1,31 @@
+
+@REM  Copyright (C) 2014 Schneider Electric
+@REM
+@REM  This file is part of "Mind Compiler" is free software: you can redistribute 
+@REM  it and/or modify it under the terms of the GNU Lesser General Public License 
+@REM  as published by the Free Software Foundation, either version 3 of the 
+@REM  License, or (at your option) any later version.
+@REM 
+@REM  This program is distributed in the hope that it will be useful, but WITHOUT 
+@REM  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+@REM  FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+@REM  details.
+@REM 
+@REM  You should have received a copy of the GNU Lesser General Public License
+@REM  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@REM 
+@REM  Contact: mind@ow2.org
+@REM 
+@REM  Authors: Schneider Electric Mind4SE
+@REM  Contributors: 
+@REM -----------------------------------------------------------------------------
+@REM
+
+@echo off
+setlocal 
+
+@REM Launcher class name
+set LAUNCHER=org.ow2.mind.ObjDepSolver
+
+echo %~dp0\jar_launcher.bat %LAUNCHER% -Dmindc.launcher.name=object-dependencies %LAUNCHER% %*
+%~dp0\jar_launcher.bat %LAUNCHER% -Dmindc.launcher.name=object-dependencies %LAUNCHER% %*
